1c7e29acd8409c3392bb94a284cab8cd893aaabc
jesus5413 <wwh124@my.utsa.edu>
2017-12-18 03:37:21 -0600

    deleting updating

--- simul/Controllers/LogInVC.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LogInVC.swift
-//  simul
-//
-//  Created by Jesus Nieto on 12/18/17.
-//  Copyright © 2017 Jesus Nieto. All rights reserved.
-//
-
-import Foundation
--- simul/Controllers/SignUpVC.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SignUpVC.swift
-//  simul
-//
-//  Created by Jesus Nieto on 12/18/17.
-//  Copyright © 2017 Jesus Nieto. All rights reserved.
-//
-
-import Foundation
